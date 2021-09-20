94c8b41d005ac755851300553481ba44072af373
yumizaayaka <aaaaykiphone@gmail.com>
2021-09-20 11:36:18 +0900

    commit2

--- themes/estra-template/header.php
+++ themes/estra-template/header.php
@@ -4,9 +4,9 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="<?php get_theme_file_uri( '/css/reset.css' );?>"
-    <link rel="stylesheet" href="<?php get_theme_file_uri( '/css/common.css' );?>"
-    <link rel="stylesheet" href="<?php get_theme_file_uri( '/style.css' ); ?>"
+    <link rel="stylesheet" href="<?php echo get_theme_file_uri( '/css/reset.css' );?>">
+    <link rel="stylesheet" href="<?php echo get_theme_file_uri( '/css/common.css' );?>">
+    <link rel="stylesheet" href="<?php echo get_theme_file_uri( '/style.css' ); ?>">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Klee+One&family=Sawarabi+Mincho&family=Shippori+Mincho:wght@500&display=swap" rel="stylesheet">
